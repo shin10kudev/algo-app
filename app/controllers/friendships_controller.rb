7e612862f181d64a67360a3cf3db39feedbc6c4d
Elliott Hindman <elliott_hindman@metaps.com>
2016-12-26 17:46:34 +0900

    fix following buttons

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -11,23 +11,27 @@ class FriendshipsController < ApplicationController
 
   def create
     @user = User.find(params[:friend_id])
+
     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+
   	if @friendship.save
       @note = @user.notifications.build(path: "users/#{current_user.id}", action: "followed you", originator_id: current_user.id, reference_id: current_user.id)
       @note.save
   		redirect_to(:back)
   	else
-  		render 'posts', alert: "Oops! something went wrong..."
+  		render @user, alert: "Oops! something went wrong..."
   	end
   end
 
   def destroy
+    @user = User.find(params[:id])
+
   	@friendship = current_user.friendships.find_by(friend_id: params[:id])
+
   	if @friendship.destroy
   		redirect_to(:back)
   	else
-  		render 'posts', alert: "Oops! something went wrong..."
+  		render @user, alert: "Oops! something went wrong..."
   	end
   end
-
 end
