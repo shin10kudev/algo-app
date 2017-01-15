8593df5c5b842067e665a3b301d7512989f6306a
Elliott Hindman <elliott_hindman@metaps.com>
2017-01-15 16:33:04 +0900

    add filter to user posts page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
+
+    if params[:category_id]
+      @posts = @posts.where(category_id: params[:category_id])
+    end
   end
 end
