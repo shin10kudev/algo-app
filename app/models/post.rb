e6eb5ee981a997ac84e69c2569e53deaa340f835
Elliott Hindman <elliott_hindman@metaps.com>
2017-01-16 17:08:43 +0900

    add validation for category

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,13 +7,14 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, length: { in: 2..30 }
   validates :description, presence: true, length: { in: 2..250 }
   validates :code, presence: true
-  validates :category_id, presence: true
   validates :user_id, presence: true
   validates :difficulty,
     presence: true,
     inclusion: { in: [ 'Easy', 'Medium', 'Hard' ],
     message: "%{value} is not a valid difficulty level" }
-
-  validates :category, presence: true
+  validates :category_id,
+    presence: true,
+    inclusion: { in: Category.all.map{ |category| category.id },
+    message: "%{value} is not a valid category" }
 
 end
