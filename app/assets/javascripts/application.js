22df60a100bdaabc473c1d86530c5b74e0509f23
Elliott Hindman <elliott_hindman@metaps.com>
2017-01-17 13:13:59 +0900

    update user account view and add better actions to posts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,10 +19,26 @@
 //= require ace/theme-monokai
 //= require ace/mode-javascript
 
+// Fade-out-remove animation for alerts
 $(document).ready(function() {
 	setTimeout(function() {
 		$('.alert-box').slideUp('slow', function() {
 			$(this).remove();
 		})
 	}, 2000);
+});
+
+// Data confirm modal interactions
+$(document).on('ready', function () {
+  var $confirmationModal = $("[data-confirm-modal]");
+
+  if($confirmationModal.length) {
+    $confirmationModal.on('click', function() {
+      var postId = $(this).attr('data-id');
+      var title = $(this).attr('data-title');
+
+      $('#destroy-button').attr("href", "/posts/" + postId);
+      $('.modal-title').html("Delete Algorithm");
+    });
+  }
 });
