0e3dab8fda6e8a68ceece42d7525b2f47fccf1b9
Elliott Hindman <elliott_hindman@metaps.com>
2017-01-17 12:31:02 +0900

    add pagination ot user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.page(params[:page]).per(15)
 
     if params[:category_id]
-      @posts = @posts.where(category_id: params[:category_id])
+      @posts = @posts.where(category_id: params[:category_id]).page(params[:page]).per(15)
     end
   end
 end
