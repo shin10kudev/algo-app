8aec1fb9c5171f86dc8cfce209ee624a6003029e
Elliott Hindman <elliott_hindman@metaps.com>
2017-01-20 18:04:50 +0900

    remove unused image column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119084019) do
+ActiveRecord::Schema.define(version: 20170120090320) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 20170119084019) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "image"
     t.text     "bio"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
